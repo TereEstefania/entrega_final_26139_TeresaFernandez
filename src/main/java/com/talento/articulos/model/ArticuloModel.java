b867d7e3187824467b965fd3a296d55e4447d0c5
TereEstefania <fernandez.e.tere@gmail.com>
2026-07-01 22:39:11 -0300

    feat: crea modelo articulo

--- src/main/java/com/talento/articulos/model/ArticuloModel.java
+++ src/main/java/com/talento/articulos/model/ArticuloModel.java
@@ -1,5 +1,71 @@
 package com.talento.articulos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+
+@Entity
+@Table(name = "articulo")
 public class ArticuloModel {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 200)
+    private String nombre;
+
+    @Column(name = "precio", nullable = false)
+    private Double precio;
+
+    @Column(name = "imagen", nullable = true, length = 500)
+    private String imagen;
+
+    public ArticuloModel(){
+
+    }
+
+    public ArticuloModel(Long id, String nombre, Double precio, String imagen){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNmbre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(Double precio){
+        this.precio = precio;
+    }
+
+    public String getImagen(){
+        return imagen;
+    }
+
+    public void setImagen(String imagen){
+        this.imagen = imagen;
+    }
 }
